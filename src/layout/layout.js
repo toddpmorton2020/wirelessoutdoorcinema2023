0528209b7e9326627ea389c872379cd5e8fe2acf
louddays <team@louddays.com>
2023-06-16 19:29:31 +0530

    second commit

---
+++ src/layout/layout.js
@@ -0,0 +1,14 @@
+import React from "react"
+import PropTypes from "prop-types"
+const Layout = ({ children }) => {
+  return (
+    <>
+      {children}
+    </>
+  )
+}
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout

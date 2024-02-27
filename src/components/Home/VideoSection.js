d23ad6a821f852ff46edcdae139f994467338784
Himanshu Rawat <himanshu091rawat@gmail.com>
2024-02-27 23:18:59 +0530

    Added fixes

--- src/components/Home/VideoSection.js
+++ src/components/Home/VideoSection.js
@@ -24,17 +24,21 @@ const VideoSection = () => {
           <div className="lg:pb-11 text-white lg:pt-0 pt-11 md:px-0 px-5">
             <div className="flex items-end">
               {/* <StaticImage src="../assets/images/timberline-big-moose-220px-x-211px.png" className="w-full"/> */}
-              <GatsbyImage
-                image={getImage(TimberlineMoose)}
-                style={{
+             <div  style={{
                   height: "200px",
                   width: "400px",
                   // margin: "auto",
                   paddingBottom:'15px'
+                }} className="hidden md:block"> <GatsbyImage
+                image={getImage(TimberlineMoose)}
+                style={{
+                 
+                  // margin: "auto",
+                  paddingBottom:'15px'
                 }}
                 alt="Timberline Moose"
-                className="hidden md:block"
-              />
+             
+              /></div>
               <div className="hidden md:block">
                 <h2 className="xl:text-50 text-2xl lg:text-5xl md:text-4xl font-semibold leading-c18 md:leading-none md:pb-3 pb-2 md:block hidden mt-8">
                   Introducing the Timberline Series Big Moose Outdoor Movie
